6750f35d1cb167891f69a8746cee7f4772f17249
poteramalcolm@gmail.com <104031265+mal-prog@users.noreply.github.com>
2022-05-12 13:46:42 -0600

    phonenumber

--- manageuser.js
+++ manageuser.js
@@ -2,14 +2,20 @@
 
 let userName = "";
 let password = "";
+let phonenumber ="";
 let verifypassword = "";
 let passwordRegEx=/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%!]).{6,40})/;
+
+function setphonenumber(){
+    phonenumber = $("#phonenumber").val();
+}
 
 function setusername(){
     userName = $("#username").val();
 }
 
 function setuserpassword(){
+
     password = $("#password").val();
     var valid=passwordRegEx.exec(password);
     if (!valid){
@@ -46,9 +52,20 @@ function checkexpiredtoken(token){
     }
 }
 
+function sendtext(){
+    setphonenumber();
+
+$.ajax({
+    type:'POST',
+    url: 'https://dev.stedi.me/twofactorlogin/'+phonenumber,
+contentType: 'application/text',
+dataType: 'text'
+})
+}
+
 function userlogin(){
     setuserpassword();
-    setusername();
+
     $.ajax({
         type: 'POST',
         url: 'https://dev.stedi.me/login',
